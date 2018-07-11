9f83d74f06604d439acda42c8d53bb95af8cbbd3
solangealmn <solangealmn@gmail.com>
2018-07-11 00:22:49 -0300

    Exercícios resolvidos em sala.

--- mini-zoo/assets/script.js
+++ mini-zoo/assets/script.js
@@ -1,1 +1,25 @@
+var dropMenu = document.getElementById('drop-menu');
+dropMenu.addEventListener('change', cliqueEfeito);
+var pegaFoto = document.querySelectorAll('.thumbs-item');
 
+function cliqueEfeito(){
+  //arrow function => setinha
+  pegaFoto.forEach(foto => {
+    var efeito = dropMenu.value;
+    if(efeito === 'sepia'){
+      foto.classList = 'thumbs-item';
+      foto.classList.add('sepia');
+    }
+    if(efeito === 'grayscale'){
+      foto.classList = 'thumbs-item';
+      foto.classList.add('grayscale');
+    }
+    if(efeito === 'invert'){
+      foto.classList = 'thumbs-item';
+      foto.classList.add('invert');
+    }
+    if(efeito === ''){
+      foto.classList = 'thumbs-item';
+    }
+  });
+}
--- programadoras/assets/script.js
+++ programadoras/assets/script.js
@@ -1,10 +1,32 @@
+var dropMenu = document.getElementById('drop-menu');
+dropMenu.addEventListener('change', carregaProgramadoras);
 
+window.onload = carregaMenu();
 
+function carregaMenu(){
+  var nome = document.createElement('option');
+  nome.innerHTML = "Selecione sede";
+  nome.value = "none";
+  dropMenu.appendChild(nome);
+  for(sede in data){
+    var itemMenu = document.createElement('option');
+    itemMenu.value = sede;
+    itemMenu.innerHTML = sede;
+    dropMenu.appendChild(itemMenu);
+  }
 
-// function carregaMenu(){
-
-// };
+};
 
 function carregaProgramadoras(){
+  var sede = dropMenu.value;
+  var listaProgramadoras = document.getElementById("exibe-programadoras");
+  listaProgramadoras.innerHTML = "";
+  for(turma in data[sede]){
+    for(i in data[sede][turma]["students"]){
+      var img = document.createElement('img');
+      img.src = data[sede][turma]["students"][i]["photo"];
+      listaProgramadoras.appendChild(img);
+    }
+  }
 
 };
